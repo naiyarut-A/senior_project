2227e11c8c688fc7c7339652b9f3e24c68ebe2e7
tharueporn <tharueporn@tharueporns-mac.local>
2018-12-27 20:47:37 -0800

    Initial Commit

---
+++ Test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Test
+//
+//  Created by tharueporn on 27/12/2561 BE.
+//  Copyright © 2561 tharueporn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Test
+//
+//  Created by tharueporn on 27/12/2561 BE.
+//  Copyright © 2561 tharueporn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
