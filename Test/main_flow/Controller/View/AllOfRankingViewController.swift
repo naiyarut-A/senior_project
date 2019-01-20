bbf9fa70459154e85b87a3522cb10d68f63d2ee7
naiyarut <naiyarut@naiyaruts-mac-2.local>
2019-01-20 05:57:24 -0800

    set cells like prototype in table view

--- Test/main_flow/Controller/View/AllOfRankingViewController.swift
+++ Test/main_flow/Controller/View/AllOfRankingViewController.swift
@@ -12,44 +12,67 @@ class AllOfRankingViewController: UIViewController, UITableViewDelegate, UITable
     
     var allrank_index = Int()
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let cellReuseIdentifier = "allofrankingcell"
+    let cellSpacingHeight: CGFloat = 5
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return Global.filterbank.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return Global.filterbank.count
+        return 1
     }
     
     // set header's table
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        //        let view = UIView()
-        let cell = tableView.dequeueReusableCell(withIdentifier: "headercell")
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "headercell")
+//
+//        return cell
+        let  headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
         
-        return cell
+        return headerView
+        
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+//        return 50
+        return cellSpacingHeight
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 45
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 45
+//    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         //SORT RATE
         let bank = Global.filterbank.sorted(){$0.rate > $1.rate}
-        let allofbank = bank[indexPath.row]
+//        let allofbank = bank[indexPath.row]
+        let allofbank = bank[indexPath.section]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "allofrankingcell", for: indexPath) as! CustomAllOfRankingTableViewCell
         let set_rate_format = String(format: "%.2f", allofbank.rate)
-        let set_receive_format = String(format: "%.2f", Global.receive_interest[indexPath.row])
+//        let set_receive_format = String(format: "%.2f", Global.receive_interest[indexPath.row])
         
+        let set_receive_format = String(format: "%.2f", Global.receive_interest[indexPath.section])
+
         cell.logo_bank.image = UIImage(named: (allofbank.img + ".png"))
         cell.type_acc.text = allofbank.typedep
         cell.rateperyear.text = String(set_rate_format)
         cell.receive_interest.text = String(set_receive_format)
         
+        // add border and color
+        cell.backgroundColor = UIColor.white
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        Global.index = indexPath.row
+        Global.index = indexPath.section
         
         performSegue(withIdentifier: "AllRanktoShowDetails", sender: self)
     }
